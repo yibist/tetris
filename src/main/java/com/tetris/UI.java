74afa69adee63c09aabc7a9ce5fcda27de7f128d
Michael Galambos <michael.galambos@students.fhnw.ch>
2024-10-01 10:31:06 +0200

    adds image code

--- src/main/java/com/tetris/UI.java
+++ src/main/java/com/tetris/UI.java
@@ -1,12 +1,13 @@
 package com.tetris;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class UI extends Application {
     public static void main(String[] args) {
@@ -14,21 +15,17 @@ public class UI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tetris");
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 300, 300));
+
+        Image tile = new Image("tile.png", 16.0d, 16.0d, true, true);
+        ImageView tileView = new ImageView(tile);
+        tileView.setX(1);
+        tileView.setY(1);
+        tileView.setFitWidth(16.0d);
+
         primaryStage.show();
     }
 }
