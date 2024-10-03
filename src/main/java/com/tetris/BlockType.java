5cd4a58494ae92d1866e273b98a8cbed30090507
Klemen Zmahar <klemenzmahar@gmail.com>
2024-10-03 08:24:40 +0200

    method to return random Blocktype

--- src/main/java/com/tetris/BlockType.java
+++ src/main/java/com/tetris/BlockType.java
@@ -1,5 +1,7 @@
 package com.tetris;
 
+import java.util.Random;
+
 public enum BlockType {
     LShape(6, "LShape.png"),
     LShapeFlipped(6, "LShapeFlipped.png"),
@@ -15,6 +17,20 @@ public enum BlockType {
     BlockType(int cornerCount, String imageLoc) {
         this.cornerCount = cornerCount;
         this.imageLoc = imageLoc;
+    }
+
+    public BlockType getRandomBlockType() throws Exception {
+        int randInt = new Random().nextInt(7);
+        switch(randInt){
+            case 0: return BlockType.LShape;
+            case 1: return BlockType.LShapeFlipped;
+            case 2: return BlockType.TShape;
+            case 3: return BlockType.Straight;
+            case 4: return BlockType.ZShape;
+            case 5: return BlockType.ZShapeFlipped;
+            case 6: return BlockType.Square;
+            default: throw new Exception("No such block type associated with number: "+randInt);
+        }
     }
 
     public int getCornerCount() {
