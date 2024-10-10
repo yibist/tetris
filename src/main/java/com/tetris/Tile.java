4911a8fb5d92368f5d77f33fb37822c593c1e3c0
Klemen Zmahar <klemenzmahar@gmail.com>
2024-10-10 16:00:37 +0200

    Tile doc written

--- src/main/java/com/tetris/Tile.java
+++ src/main/java/com/tetris/Tile.java
@@ -5,16 +5,42 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class Tile {
+    /**
+     * x position of the tile.
+     */
     public int x;
+    /**
+     * y position of the tile.
+     */
     public int y;
+    /**
+     * Size of the tile in pixels
+     * <br>**Used in many locations including screen size and collisions**<br>
+     * **CHANGE AT OWN RISK**
+     */
     public static final int size = 32;
+    /**
+     * Tile image sprite
+     */
     public static Image image = new Image("Tile.png", Tile.size, Tile.size, true, true);
 
+    /**
+     * **ALL PROVIDED PARAMETERS WILL BE MULTIPLIED BY SIZE**
+     * @param x
+     * x position of tile
+     * @param y
+     * y position of tile
+     */
     Tile(int x, int y) {
         this.x = x * size;
         this.y = y * size;
     }
 
+    /**
+     * Draw Tile on pane
+     * @param pane
+     * Pane tile will be drawn on
+     */
     public void drawTile(Pane pane) {
         ImageView imageView = new ImageView(image);
         imageView.setX(this.x);
@@ -23,6 +49,11 @@ public class Tile {
         pane.getChildren().add(imageView);
     }
 
+    /**
+     * Moves the tile in the specified direction.
+     * @param direction
+     * The direction of movement valid inputs are "left", "right" and "down". This parameter is not case-sensitive.
+     */
     public void move(String direction) {
         switch ((direction.toLowerCase())) {
             case "down":
